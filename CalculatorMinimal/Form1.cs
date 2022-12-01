87a27c82f6dc5c85de4155aeccf8d333ee9bd632
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2022-12-01 22:50:31 +0200

    Add project files.

---
+++ CalculatorMinimal/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CalculatorMinimal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double a = double.Parse(textBox1.Text);
+                double b = double.Parse(textBox2.Text);
+                double c = 0;
+
+                int op = comboBox1.SelectedIndex;
+
+
+                switch (op)
+                {
+                    case 0: c = a + b; break;
+                    case 1: c = a - b; break;
+                    case 2: c = a * b; break;
+                    case 3: c = a / b; break;
+
+                }
+
+                textBox3.Text = c.ToString();
+            }
+            catch { }
+        }
+    }
+}
